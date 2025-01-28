1767941ff2cccb0eb14be2407f1444e61d4249f8
jmbillard <jmbillard2@gmail.com>
2025-01-28 16:16:03 -0300

    -

--- ABRIDOR.js
+++ ABRIDOR.js
@@ -329,70 +329,76 @@ function ABRIDOR_UI() {
 
 		for (var i = 0; i < structureObj.elements.length; i++) {
 	
-			var element = structureObj.elements[i];
-	
-			if (!element.Text) continue;
-			if (element.Path.match(/Figure/i)) continue;
-	
-			var page = element.Page;
-			var bounds = element.Bounds;
-			var pageW = structureObj.pages[page].width;
-			var pageH = structureObj.pages[page].height;
-			var fW = compW / pageW;
-			var fH = compH / pageH;
-			f = Math.min(fW, fH);
-	
-			var currentText = comp.layers.addBoxText([(bounds[2] - bounds[0]) * fW, ((bounds[3] - bounds[1]) * fH) + 5]);
-	
-			var textProp = currentText.property("ADBE Text Properties").property("ADBE Text Document");
-			var textDoc = textProp.value;
-			textDoc.resetCharStyle();
-	
-			var txtProp = {
-				text: element.Text,
-				fontSize: element.TextSize * f,
-				justification: 7413,
-				tracking: 0
-			};
-			if (element.attributes.TextAlign == 'Center') txtProp.justification = 7415;
-			if (element.attributes.TextAlign == 'End') txtProp.justification = 7414;
-			if (element.attributes.LineHeight != undefined) txtProp.leading = element.attributes.LineHeight * fH;
-	
-			for (var p in txtProp) {
-	
-				textDoc[p] = txtProp[p];
-				textProp.setValue(textDoc);
-			}
-	
 			try {
-				textDoc.font = element.Font.name.replace(/^.+\+/, '');
-				textProp.setValue(textDoc);
+
+				var element = structureObj.elements[i];
+		
+				if (!element.Text) continue;
+				if (element.Path.match(/Figure/i)) continue;
+		
+				var page = element.Page;
+				var bounds = element.Bounds;
+				var pageW = structureObj.pages[page].width;
+				var pageH = structureObj.pages[page].height;
+				var fW = compW / pageW;
+				var fH = compH / pageH;
+				f = Math.min(fW, fH);
+		
+				var currentText = comp.layers.addBoxText([(bounds[2] - bounds[0]) * fW, ((bounds[3] - bounds[1]) * fH) + 5]);
+		
+				var textProp = currentText.property("ADBE Text Properties").property("ADBE Text Document");
+				var textDoc = textProp.value;
+				textDoc.resetCharStyle();
+		
+				var txtProp = {
+					text: element.Text,
+					fontSize: element.TextSize * f,
+					justification: 7413,
+					tracking: 0
+				};
+				if (element.attributes.TextAlign == 'Center') txtProp.justification = 7415;
+				if (element.attributes.TextAlign == 'End') txtProp.justification = 7414;
+				if (element.attributes.LineHeight != undefined) txtProp.leading = element.attributes.LineHeight * fH;
+		
+				for (var p in txtProp) {
+		
+					textDoc[p] = txtProp[p];
+					textProp.setValue(textDoc);
+				}
+		
+				try {
+					textDoc.font = element.Font.name.replace(/^.+\+/, '');
+					textProp.setValue(textDoc);
+				} catch (err) {
+					textDoc.font = 'ArialMT';
+					textProp.setValue(textDoc);
+					textDoc.fontSize *= 0.8;
+					textProp.setValue(textDoc);
+				}
+		
+				// transformations...
+				var transform = currentText.property('ADBE Transform Group');
+		
+				var posX = (bounds[0] + (bounds[2] - bounds[0]) / 2) * fW;
+				var posY = (pageH - bounds[1] - (bounds[3] - bounds[1]) / 2) * fH;
+				transform.property('ADBE Position').setValue([posX, posY, 0]);
+		
+				currentText.moveToEnd();
+		
+				layerInPoint = page * layerDuration;
+				layerOutPoint = layerInPoint + layerDuration;
+				currentText.inPoint = layerInPoint;
+				currentText.outPoint = layerOutPoint;
+				textArray.push(currentText);
+		
+				progressBar.value ++;
+				progressLab.text = 'pág. ' + progressBar.value + ',   texto: ' + element.Text;
+				comp.time = layerInPoint;
+				ABRIDOR_w.update();
 			} catch (err) {
-				textDoc.font = 'ArialMT';
-				textProp.setValue(textDoc);
-				textDoc.fontSize *= 0.8;
-				textProp.setValue(textDoc);
+
+				alert('erro ao criar o texto: ' + element.Text + '\n' + err.message);
 			}
-	
-			// transformations...
-			var transform = currentText.property('ADBE Transform Group');
-	
-			var posX = (bounds[0] + (bounds[2] - bounds[0]) / 2) * fW;
-			var posY = (pageH - bounds[1] - (bounds[3] - bounds[1]) / 2) * fH;
-			transform.property('ADBE Position').setValue([posX, posY, 0]);
-	
-			currentText.moveToEnd();
-	
-			layerInPoint = page * layerDuration;
-			layerOutPoint = layerInPoint + layerDuration;
-			currentText.inPoint = layerInPoint;
-			currentText.outPoint = layerOutPoint;
-			textArray.push(currentText);
-	
-			progressBar.value ++;
-			progressLab.text = 'pág. ' + progressBar.value + ',   texto: ' + element.Text;
-			comp.time = layerInPoint;
-			ABRIDOR_w.update();
 		}
 	
 		var refFolder = app.project.items.addFolder('ABERTURA REF'); // Cria uma pasta no projeto.
